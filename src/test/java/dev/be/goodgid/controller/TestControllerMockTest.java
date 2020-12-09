89308aeee288ddbb81b4868b693c7ab2719c2cb6
goodGid <hello.goodgid@gmail.com>
2020-12-09 09:14:23 +0900

    Add Jenkinsfile

---
+++ src/test/java/dev/be/goodgid/controller/TestControllerMockTest.java
@@ -0,0 +1,16 @@
+package dev.be.goodgid.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestControllerMockTest {
+
+
+    @Test
+    public void test() throws Exception {
+        System.out.println("Test Success");
+
+    }
+
+}
